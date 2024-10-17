621a3e9a7cff91a41fe2bb4f2840dd991cab2a21
Homayun Gohari <homayuunz98@gmail.com>
2024-10-17 12:05:40 +0330

    First

---
+++ Coordinator Pattern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coordinator Pattern
+//
+//  Created by Homayun on 7/26/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Coordinator Pattern/Coordinator_PatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator_PatternApp.swift
+//  Coordinator Pattern
+//
+//  Created by Homayun on 7/26/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct Coordinator_PatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
